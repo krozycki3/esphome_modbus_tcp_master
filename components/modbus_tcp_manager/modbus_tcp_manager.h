3dd91eba53c1d289e123f075936eb7aba274b6ab
krozycki3 <47122301+krozycki3@users.noreply.github.com>
2026-09-09 13:07:59 +0200

    Update modbus_tcp_manager.h
    delay 1500

--- components/modbus_tcp_manager/modbus_tcp_manager.h
+++ components/modbus_tcp_manager/modbus_tcp_manager.h
@@ -334,7 +334,7 @@ private:
                         ::getsockopt(connection_check_sock_, SOL_SOCKET, SO_ERROR, &error, &len);
                         
                         if (error == 0) {
-                            delay(2100);
+                            delay(1500);
                             ESP_LOGV(TAG, "Connection check succeeded");
                             connection_check_success_ = true;
                         } else {
